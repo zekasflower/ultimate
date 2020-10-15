562a82780b45443a52a20396d067d5a679fbc87f
zekasflower <zekas.flower@gmail.com>
2020-10-15 15:54:36 +0200

    Sub menu

--- dist/js/ultimate-min.js
+++ dist/js/ultimate-min.js
@@ -1,1 +1,1 @@
-$(function(){"use strict";$(".toggle-sidebar").on("click",function(){$(".content-area, .sidebar").toggleClass("no-sidebar")})});
+$(function(){"use strict";$(".toggle-sidebar").on("click",function(){$(".content-area, .sidebar").toggleClass("no-sidebar")}),$("li.has-sub").on("click",function(){$(this).find(".chevron-forward-outline").toggleClass("rotating"),$(this).find(".sub-links").slideToggle()})});
